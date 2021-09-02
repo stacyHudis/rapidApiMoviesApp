5d45de314643c2692a757a5d4c743eb5eed96498
stacy <stacyhudis@gmail.com>
2021-09-02 18:13:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# RapidApiMoviesApp
+
+### Install Dependencies
+
+```
+npm install
+```
+
+### Run the Application
+
+Start server using:
+
+```
+npm start
+```
+
+Browse to the app at localhost:3000
---
+++ app.js
@@ -0,0 +1,39 @@
+//import modules
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+// Loads env variables
+require('dotenv').config();
+
+//Create app
+const app = express();
+
+//port
+const port = process.env.PORT || 3000;
+
+const route = require('./routes/routes');
+
+//json parse
+app.use(express.json());
+
+//add CORS support -
+//FrontEnd is using localhost:8000
+//BackEnd is using localhost:3000
+//Need to allow cross-origin resource sharing
+app.use(cors());
+
+//Paths
+const viewsPath = path.join(__dirname, './public');
+
+// view engine setup
+app.set('view engine', 'pug');
+app.set('views', viewsPath);
+
+//routes
+app.use('/api', route);
+
+//listen
+app.listen(port, ()=>{
+   console.log('Server listening at port:' + port);
+});
